a6f9301379db7e33ec52b072b6bb77bc1d944187
Tee <cickpoo0121@gmail.com>
2021-11-04 18:02:55 +0700

    ver 1.0.0

--- index.js
+++ index.js
@@ -9,42 +9,89 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const line = require('@line/bot-sdk');
+
 // variable
 const configKey = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
 const pushMsgUrl = 'https://api.line.me/v2/bot/message/push'
 
-app.get('/sendMessage', (req, res) => {
-    // const targetId = req.body.targetId
+const client = new line.Client({
+    channelAccessToken: '' + configKey.channelAccessToken + ''
+});
 
-    // send message function
-    pushMessage('U51f8ec6105489185ed5ceb883f4d5fbc')
+const messageOver = [
+    {
+        type: 'text',
+        text: '$ ปริมาณสารอาหารเกินกำหนดแล้วนะ อย่าลืมออกกำลังกายด้วยนะ',
+        emojis: [
+            {
+                "index": 0,
+                "productId": "5ac21a18040ab15980c9b43e",
+                "emojiId": "048"
+            },
+        ],
+
+    },
+    {
+        type: "sticker",
+        packageId: "446",
+        stickerId: "2000",
+    }
+];
+
+const messageIncase = [
+    {
+
+        type: 'text',
+        text: '$ สุดยอดไปเลย $ \n ปริมาณสารอาหารอยู่ในเกณฑ์ \n พยายามควยคุมอย่าให้เกินนะ',
+
+        emojis: [
+            {
+                "index": 0,
+                "productId": "5ac223c6040ab15980c9b44a",
+                "emojiId": "038"
+            },
+            {
+                "index": 14,
+                "productId": "5ac223c6040ab15980c9b44a",
+                "emojiId": "035"
+            },
+        ],
+
+    },
+    {
+        type: "sticker",
+        packageId: "11538",
+        stickerId: "51626507",
+    }
+];
+
+
+app.post('/sendMessage', (req, res) => {
+    // const userId = req.body.userId;
+    // const event = req.body.event;
+    // const { userId, event } = req.body;
+    const { userId, event } = req.body;
+
+    console.log(req.body)
+    // console.log(event)
+
+    client.pushMessage(userId, event == 0 ? messageIncase : messageOver)
+        .then(() => {
+            console.log('success')
+        })
+        .catch((err) => {
+            // error handling
+            console.log(err)
+        });
+
     res.send('success').status(200)
 })
 
-function pushMessage() {
-    let headers = {
-        'Conten-Type': 'application/json',
-        'Authorization': `Bearer ${configKey.channelAccessToken}`
-    }
-    let body = JSON.stringify({
-        message: [{
-            type: 'text',
-            text: 'Test Elder Eat line Notify'
-        }]
-    })
 
-    request.post({
-        url: pushMsgUrl,
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    })
-}
-
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('404')
 })
 
